f83bef2d214f8af54f411e8dca9f771d81717ff0
Yashraj9983 <67761589+Yashraj9983@users.noreply.github.com>
2023-05-23 11:31:26 +0530

    Add files via upload

--- assignment2/index.js
+++ assignment2/index.js
@@ -1,3 +1,11 @@
+baseURL="http://13.233.112.16:5000/seatinfo"
+function fetchData() {
+fetch(baseURL).then(data => {
+   // use data here
+   console.log(data)
+})
+}
+fetchData();
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
